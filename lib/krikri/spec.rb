e08027f3c14cfb114c9379032f8192702098cc94
Tom Johnson <tom@dp.la>
2016-06-09 10:44:09 -0700

    Add RSpec matchers
    Move ActiveTriples pattern matchers over from Krikri.

--- lib/krikri/spec.rb
+++ lib/krikri/spec.rb
@@ -6,14 +6,32 @@
 # with clarity of shared example use, and avoids namespace pollution for 
 # `it_behaves_like` directives.
 # 
-# @example
+# @example using a shared example
 #   require 'krikri/spec/harvester'
 #
 #   describe MyHarvester do
 #     it_behaves_like 'a harvester'
 #   end
 #
+#
+# RSpec matchers are included for common checks within Krikri based applications.
+#
+# @example using a custom matcher
+#   require 'krikri/spec'
+#    
+#   RSpec.configure { |conf| conf.include(Krikri::Spec::Matchers) }
+#
+#   describe SomeModel do
+#     it 'is an exactMatch for moomin' do
+#       moomin = RDF::URI('http://example.com/moomin')
+#
+#       expect(subject).to be_exact_match_with moomin
+#     end
+#   end
+#
+# @see http://www.rubydoc.info/gems/krikri
+# @see http://rspec.info/
 module Krikri::Spec
-  autoload :VERSION, 'krikri/spec/version'
-
+  autoload :Matchers, 'krikri/spec/matchers'
+  autoload :VERSION,  'krikri/spec/version'
 end
