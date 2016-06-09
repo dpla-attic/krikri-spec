6c6ee251f0220888b4ed54e35ad00f5ca60846c5
Tom Johnson <tom@dp.la>
2016-06-09 09:49:07 -0700

    Add harvester shared example
    Moves `harvester.rb` from the main Krikri repository as an initial
    shared example. Adds documentation for shared example usage to the main
    module.

--- lib/krikri/spec.rb
+++ lib/krikri/spec.rb
@@ -2,6 +2,18 @@
 # `Krikri::Spec` offers RSpec extensions and shared examples for the Krikri 
 # metadata aggregation engine.
 #
+# Shared examples must be required explictly for specs that use them. This helps
+# with clarity of shared example use, and avoids namespace pollution for 
+# `it_behaves_like` directives.
+# 
+# @example
+#   require 'krikri/spec/harvester'
+#
+#   describe MyHarvester do
+#     it_behaves_like 'a harvester'
+#   end
+#
 module Krikri::Spec
+  autoload :VERSION, 'krikri/spec/version'
 
 end
